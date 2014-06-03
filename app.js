de7a6c3fdeb6e04e28324b0c4b6ed9ffcbb523bc
Steve Hackbarth <stephenhackbarth@gmail.com>
2014-06-02 22:53:10 -0400

    add footer

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 var express = require('express');
 var path = require('path');
 var app = express();
+var port = Number(process.env.PORT || 3000);
 
 app.set('view engine', 'ejs');
 app.get('/', function (req, res){
   res.redirect("./the-gettysburg-sequence.md");
-  //res.render('content', {markdown: "hithere"});
-  //res.send('Hello World');
 });
 app.use(require('express-markdown')({
   directory: __dirname + '/content',
@@ -16,7 +15,6 @@ app.use(require('express-markdown')({
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
-var port = Number(process.env.PORT || 3000);
 app.listen(port, function () {
   console.log("Listening on " + port);
 });
