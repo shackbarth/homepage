122cbe700640a32ace625b538b1d9b9f518ffa09
Steve Hackbarth <stephenhackbarth@gmail.com>
2014-06-09 22:11:05 -0400

    break up the logic

--- app.js
+++ app.js
@@ -1,61 +1,38 @@
-var _ = require("underscore"),
-  async = require('async'),
-  express = require('express'),
-  fs = require('fs'),
-  path = require('path'),
-  port = Number(process.env.PORT || 3000),
-  app = express();
+(function () {
 
-var marked = require("marked");
+  var _ = require("underscore"),
+    express = require('express'),
+    fs = require('fs'),
+    path = require('path'),
+    port = Number(process.env.PORT || 3000),
+    app = express();
 
-app.set('view engine', 'ejs');
+  var setRoutes = function (err, results) {
+    var sitePages = _.compact(results);
+    _.each(sitePages, function (page) {
+      if (page.file === "index") {
+        app.get("/", function (req, res) {
+          res.render("index", _.extend(page, {
+            pages: _.without(sitePages, _.findWhere(sitePages, {file: "index"}))
+          }));
+        });
+      } else {
+        console.log("loading page /" + page.file);
+        app.get("/" + page.file, function (req, res) {
+          res.render("content", page);
+        });
+      }
+    });
 
-var contentFiles = fs.readdirSync(path.join(__dirname, "content"));
-var loadContentFile = function (filename, done) {
-  if (!_.contains([".markdown", ".md"], path.extname(filename))) {
-    done();
-    return;
-  }
+    app.listen(port, function () {
+      console.log("Listening on " + port);
+    });
+  };
 
-  fs.readFile(path.join(__dirname, "content", filename), "utf8", function (err, data) {
-    if (err) {
-      next(err);
-      return;
-    }
+  app.set('view engine', 'ejs');
 
-    var dataLines = data.split("\n");
-    var title = dataLines[0].substring(1).trim();
-    var subtitle = dataLines[1].substring(3).trim();
-    var detitledData = dataLines.splice(2).join("\n");
-    var markdown = marked(detitledData);
-    var basename = path.basename(filename, path.extname(filename));
-    var context = {
-      markdown: markdown,
-      file: basename,
-      title: title,
-      subtitle: subtitle
-    };
-    if (basename !== 'index') {
-      console.log("loading page /" + basename);
-      app.get("/" + basename, function (req, res) {
-        res.render("content", context);
-      });
-    }
-    done(null, context);
-  });
-};
-var afterContentFilesLoaded = function (err, results) {
-  var processedResults = _.compact(results);
-  var indexResults = _.findWhere(processedResults, {file: "index"});
-  processedResults = _.without(processedResults, indexResults);
-  app.get("/", function (req, res) {
-    res.render("index", _.extend(indexResults, {pages: processedResults}));
-  });
-  app.listen(port, function () {
-    console.log("Listening on " + port);
-  });
-};
 
-app.use(require('stylus').middleware(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'public')));
-async.map(contentFiles, loadContentFile, afterContentFilesLoaded);
+  app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+  app.use(express.static(path.join(__dirname, 'public')));
+  require("./lib/load").loadContentFiles(setRoutes);
+}());
